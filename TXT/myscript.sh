e2c7ff22a478a006b86caa471022a45b4a0374ba
Johanes Steven <sdfahsefgjk@gmail.com>
2020-10-03 10:49:50 +0700

    myscript

---
+++ TXT/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# johanessteven19
+
+FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+SHA="SHA256SUM"
+
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
+
+# Sat Oct 03 10:48:38 WIB 2020
